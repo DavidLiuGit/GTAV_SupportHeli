caabae5b0182d66aa4dd518e7b1c420b11ca68e1
David Liu <dvdgege5@gmail.com>
2020-01-30 23:00:10 -0800

    implement rappeling ground crew gunners

---
+++ SupportHeli/CrewHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GTA;
+using GTA.Math;
+
+
+namespace GFPS
+{
+	class CrewHandler
+	{
+		#region groundCrew
+		static float regroupThreshold = 15.0f;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="gunner"></param>
+		/// <param name="?"></param>
+		public static GroundCrewAction groundGunnerHandler (Ped gunner, GroundCrewAction currAction){
+			GroundCrewAction newAction = currAction;
+			Vector3 playerPos = Game.Player.Character.Position;
+
+			switch (currAction)
+			{
+				// if the gunner is currently in the air (i.e. not on the ground), do nothing
+				case GroundCrewAction.Descending:
+					if (!gunner.IsInAir) newAction = GroundCrewAction.Regrouping;
+					break;
+
+
+				// calculate 
+				case GroundCrewAction.Regrouping:
+					if (playerPos.DistanceTo(gunner.Position) > regroupThreshold){
+						gunner.BlockPermanentEvents = true;
+						gunner.Task.RunTo(playerPos);
+					}
+					else {
+						gunner.BlockPermanentEvents = false;
+						gunner.Task.FightAgainstHatedTargets(50000);
+						newAction = GroundCrewAction.Fighting;
+					}
+					break;
+
+				case GroundCrewAction.Fighting:
+					if (playerPos.DistanceTo(gunner.Position) > regroupThreshold)
+						newAction = GroundCrewAction.Regrouping;
+					break;
+			}
+
+			return newAction;
+		}
+
+
+		public static GroundCrewAction groundGunnerHandler(KeyValuePair<Ped, GroundCrewAction> entry)
+		{
+			return groundGunnerHandler(entry.Key, entry.Value);
+		}
+
+		#endregion
+	}
+
+
+
+	public enum GroundCrewAction : int
+	{
+		Descending,
+		Regrouping,
+		Fighting,
+	}
+}
--- SupportHeli/SupportHeli.cs
+++ SupportHeli/SupportHeli.cs
@@ -51,8 +51,20 @@ namespace GFPS
 		{
 			if (e.KeyCode == activateKey)
 			{
-				supportHeli = spawnSupportHeli(heliModel, supportHeliHeight, haloRadius, heliBulletProof);
-				spawnNpcsIntoHeli(supportHeli, heliModel, fPatt);
+				// if no heli is active, spawn a heli and its crew
+				if (!heliActive)
+				{
+					supportHeli = spawnSupportHeli(heliModel, supportHeliHeight, haloRadius, heliBulletProof);
+					spawnNpcsIntoHeli(supportHeli, heliModel, fPatt);
+					GTA.UI.Notification.Show("Support heli and crew spawned.");
+				}
+
+				// if a heli is already active, spawn gunners and let them rappel down
+				else
+				{
+					gunnersRappelDown(supportHeli);
+					GTA.UI.Notification.Show("Gunners rappeling.");
+				}
 			}
 		}
 
@@ -76,17 +88,26 @@ namespace GFPS
 			{
 				// if heli is destroyed (undriveable), delete its blip and set heliActive to false
 				if (!supportHeli.IsDriveable)
-					cleanUp(sender, e);
+					cleanUp(false);
 
 				try
 				{
 					// make the driver chase to player's offset
 					supportHeli.Driver.Task.ChaseWithHelicopter(Game.Player.Character, getOffsetVector3(supportHeliHeight, haloRadius));
 					supportHeli.Driver.AlwaysKeepTask = true;
+
+					// handle ground crew actions
+					var keys = groundCrew.Keys;
+					for (int i = 0; i < keys.Count; i++ )
+					{
+						Ped key = keys.ElementAt(i);
+						groundCrew[key] = CrewHandler.groundGunnerHandler(key, groundCrew[key]);
+					}
 				}
 				catch
 				{
 					cleanUp(sender, e);
+					throw;
 				}
 			}
 		}
@@ -102,7 +123,7 @@ namespace GFPS
 		float supportHeliHeight = 40.0f;
 		readonly Vector3 nullVector3 = new Vector3(0f, 0f, 0f);
 		bool heliBulletProof = true;
-		const WeaponHash defaultPrimary = WeaponHash.AdvancedRifle;
+		const WeaponHash defaultPrimary = WeaponHash.SpecialCarbine;
 		const WeaponHash defaultSidearm = WeaponHash.CombatPistol;
 		RelationshipGroup heliRelGroup;
 
@@ -161,18 +182,16 @@ namespace GFPS
 			{
 				case SupportHeliModel.Akula:
 					spawnCopilot(heli, heliRelGroup);
-					spawnGunners(heli, model, heliRelGroup);
 					break;
 
 				case SupportHeliModel.Valkyrie:
 					spawnCopilot(heli, heliRelGroup);
-					spawnGunners(heli, model, heliRelGroup, WeaponHash.AdvancedRifle);
+					spawnGunners(heli, 2, heliRelGroup, gunnerSeats);
 					break;
 
 				case SupportHeliModel.Buzzard:
-					spawnGunners(heli, model, heliRelGroup, WeaponHash.AdvancedRifle);
+					spawnGunners(heli, 2, heliRelGroup, gunnerSeats);
 					break;
-
 			}
 		}
 
@@ -180,41 +199,46 @@ namespace GFPS
 
 		private void spawnCopilot(Vehicle heli, RelationshipGroup rg, WeaponHash sidearm = defaultSidearm)
 		{
-				// spawn copilot & set into heli
-				Ped coplt = World.CreatePed(PedHash.Pilot02SMM, nullVector3);
-				coplt.SetIntoVehicle(heli, VehicleSeat.Passenger);
+			// spawn copilot & set into heli
+			Ped coplt = heli.CreatePedOnSeat(VehicleSeat.Passenger, PedHash.Pilot02SMM);
 
-				// set relationship group to player's group
-				coplt.RelationshipGroup = rg;
-				Game.Player.Character.PedGroup.Add(coplt, false);
+			// set relationship group to player's group
+			coplt.RelationshipGroup = rg;
+			Game.Player.Character.PedGroup.Add(coplt, false);
 
-				// give copilot sidearm
-				coplt.Weapons.Give(defaultSidearm, 9999, true, true);
+			// give copilot sidearm
+			coplt.Weapons.Give(defaultSidearm, 9999, true, true);
 
-				// task copilot with shooting baddies
-				coplt.Task.FightAgainstHatedTargets(50000);
-				coplt.AlwaysKeepTask = true;
+			// task copilot with shooting baddies
+			coplt.FiringPattern = FiringPattern.FullAuto;
+			coplt.Task.FightAgainstHatedTargets(50000);
+			coplt.AlwaysKeepTask = true;
 		}
 
 
 
-		private void spawnGunners(Vehicle heli, SupportHeliModel model, RelationshipGroup rg, WeaponHash weapon = defaultPrimary, WeaponHash sidearm = WeaponHash.CombatPistol)
+		VehicleSeat[] gunnerSeats = new VehicleSeat[2] { VehicleSeat.LeftRear, VehicleSeat.RightRear };
+		private Ped[] spawnGunners(Vehicle heli, int numGunners, RelationshipGroup rg, VehicleSeat[] seats, WeaponHash weapon = defaultPrimary, WeaponHash sidearm = WeaponHash.CombatPistol)
 		{
-			// determine how many gunners are needed
-			int numGunners = 0;
-			switch (model){
-				case SupportHeliModel.Buzzard:
-				case SupportHeliModel.Valkyrie:
-					numGunners = 2;
-					break;
-			}
+			Ped[] ret = new Ped[numGunners];
 
-			// spawn gunners
+			// spawn gunners as needed
 			for (int i = 0; i < numGunners; i++)
 			{
-				// spawn the gunner and put into a seat
-				Ped gunner = World.CreatePed(PedHash.Blackops01SMY, nullVector3);
-				gunner.SetIntoVehicle(heli, VehicleSeat.Any);
+				// spawn the gunner and put into a seat; if seat is occupied, add the Ped to the array
+				Ped seatOccupant = heli.GetPedOnSeat(seats[i]);
+				if (seatOccupant != null) seatOccupant.Delete();
+				Ped gunner = World.CreatePed(PedHash.Blackops01SMY, getVector3NearPlayer());
+
+				// get the gunner into the player's PedGroup; make sure player is the group's leader
+				PedGroup playerPg = Game.Player.Character.PedGroup;
+				Function.Call(Hash.SET_PED_AS_GROUP_LEADER, Game.Player.Character, playerPg);
+				playerPg.Add(gunner, false);
+				Function.Call(Hash.SET_PED_CAN_TELEPORT_TO_GROUP_LEADER, gunner.Handle, playerPg.Handle, true);
+				gunner.NeverLeavesGroup = true;
+
+				// put the gunner into a heli seat
+				gunner.SetIntoVehicle(heli, seats[i]);
 
 				// add to heli RelationshipGroup
 				gunner.RelationshipGroup = rg;
@@ -227,7 +251,12 @@ namespace GFPS
 				gunner.FiringPattern = FiringPattern.FullAuto;
 				gunner.Task.FightAgainstHatedTargets(50000);
 				gunner.AlwaysKeepTask = true;
+
+				// add the new gunner to return array
+				ret[i] = gunner;
 			}
+
+			return ret;
 		}
 
 
@@ -261,19 +290,61 @@ namespace GFPS
 
 
 
+		Dictionary<Ped, GroundCrewAction> groundCrew = new Dictionary<Ped, GroundCrewAction>(101);
+
+		private void gunnersRappelDown(Vehicle heli, int health = 1000, bool canRagdoll = false)
+		{
+			// make sure there are gunners in the gunner seats
+			Ped[] gunners = spawnGunners(heli, 2, heliRelGroup, gunnerSeats);
+
+
+
+			foreach (Ped gunner in gunners)
+			{
+				// add each gunner to the player's PedGroup
+				gunner.CanRagdoll = canRagdoll;
+				gunner.Health = health;
+
+				// make gunner rappel
+				gunner.Task.RappelFromHelicopter();
+
+				groundCrew.Add(gunner, GroundCrewAction.Descending);
+			}
+		}
+
+
+
+		private Vector3 getVector3NearPlayer (float radius = 2.0f)
+		{
+			return getOffsetVector3(0.0f, radius);
+		}
+
+
+
 		private void cleanUp(object sender, EventArgs e)
+		{
+			cleanUp(true);
+		}
+
+
+		private void cleanUp(bool deleteHeli)
 		{
 			heliActive = false;
 			try
 			{
 				supportHeli.AttachedBlip.Delete();
-				supportHeli.Delete();
+
+				// if deleteHeli flag is true, then delete all occupants and the heli itself
+				if (deleteHeli)
+				{
+					foreach (Ped occupant in supportHeli.Occupants)
+						occupant.Delete();
+					supportHeli.Delete();
+				}
+
 			}
-			catch {}
+			catch { }
 		}
-
-
-
 	}
 
 
@@ -283,6 +354,8 @@ namespace GFPS
 		Akula = 0x46699F47,
 		Valkyrie = -1600252419,
 		Buzzard = 0x2F03547B,
+		Maverick = -1660661558,
+		Polmav = 353883353,
 	}
 }
 
