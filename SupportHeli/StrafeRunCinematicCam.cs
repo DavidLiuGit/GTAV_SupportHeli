d31ffff9a088bbb4e2ec8803851954651416fd10
David Liu <dvdgege5@gmail.com>
2020-04-02 18:24:14 -0700

    add StrafeRunCinematicCam.cs to compile

--- SupportHeli/StrafeRunCinematicCam.cs
+++ SupportHeli/StrafeRunCinematicCam.cs
@@ -4,9 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SupportHeli
+using GTA;
+using GTA.Math;
+
+
+namespace GFPS
 {
-	class StrafeRunCinematicCam
+	public class StrafeRunCinematicCamController
 	{
+		#region properties
+		private Random rng = new Random();
+
+		// 
+		#endregion
+
+
+
+
+		#region constructorDestructor
+		public StrafeRunCinematicCamController()
+		{
+
+		}
+
+
+		public void destructor()
+		{
+			World.RenderingCamera = null;
+			World.DestroyAllCameras();
+		}
+		#endregion
+
+
+
+
+		public void initializeCinematicCamSequence(List<Vehicle> vehicles, List<Ped> targets, Vector3 targetPos)
+		{
+
+		}
+
+
+		#region sequences
+
+
+		#endregion
+	}
+
+
+
+
+
+	public struct StrafeRunCinematicCamSequence
+	{
+		public struct CameraTransition
+		{
+			public int duration;
+			public int easePosition;
+			public int easeRotation;
+		}
+
+		public CameraTransition[] transitionArray;
 	}
 }
