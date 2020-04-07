28a604c20902a1d8eb870a6a14bc0746a8e74d02
David Liu <dvdgege5@gmail.com>
2020-04-07 13:32:26 -0700

    add cineCam sequence 4: fly-by, 1st person, player look target

--- SupportHeli/StrafeRunCinematicCam.cs
+++ SupportHeli/StrafeRunCinematicCam.cs
@@ -72,7 +72,6 @@ namespace GFPS
 			// randomly select a sequence from available sequences
 			int randIdx = rng.Next(0, _sequences.Length);
 			StrafeRunCinematicCam[] selectedSequence = _sequences[randIdx];
-			//GTA.UI.Notification.Show("activating cine cam sequence " + randIdx);
 			_activeSequence = new Queue<SRCC>(selectedSequence);
 
 			// initialize an duplicated GameplayCamera, and render from it
@@ -139,6 +138,13 @@ namespace GFPS
 				new SRCC(SRCC_CCT.PlayerLookAtStrafeVeh, new SRCC_CT(2000, 500, 25, 25)),
 				new SRCC(SRCC_CCT.FollowStrafeVeh, new SRCC_CT(int.MaxValue, 1250, 25, 25)),
 			},
+
+			// strafeVeh fly-by, 1st person cockpit, then player look at target
+			new StrafeRunCinematicCam[] {
+				new SRCC(SRCC_CCT.StrafeVehFlyBy, new SRCC_CT(2000, 1000, 25, 25)),
+				new SRCC(SRCC_CCT.StrafeVehFirstPerson, new SRCC_CT(5000, 1000, 25, 25)),
+				new SRCC(SRCC_CCT.PlayerLookAtTarget, new SRCC_CT(int.MaxValue, 1000, 25, 25)),
+			}
 		};
 		#endregion
 
@@ -226,6 +232,7 @@ namespace GFPS
 			PlayerLookAtStrafeVeh,
 			TargetLookAtStrafeVeh,
 			StrafeVehFlyBy,
+			StrafeVehFirstPerson,
 		}
 		public cinematicCamType _type;
 		#endregion
@@ -255,6 +262,9 @@ namespace GFPS
 		{
 			switch (_type)
 			{
+				case cinematicCamType.StrafeVehFirstPerson:
+					return createStrafeVehFirstPerson(srps.vehicles[0], srps.targetPos);
+
 				case cinematicCamType.PlayerLookAtStrafeVeh:
 					return createPlayerLookAtStrafeVehCam(srps.vehicles[0]);
 
@@ -273,6 +283,14 @@ namespace GFPS
 			}
 		}
 
+
+		private Camera createStrafeVehFirstPerson(Vehicle veh, Vector3 targetPos)
+		{
+			Camera cam = World.CreateCamera(Vector3.Zero, Vector3.Zero, 60f);
+			cam.AttachTo(veh.Driver, new Vector3(0f, 0.15f, 0.75f));
+			cam.PointAt(targetPos);
+			return cam;
+		}
 
 		private Camera createPlayerLookAtStrafeVehCam(Vehicle veh)
 		{
